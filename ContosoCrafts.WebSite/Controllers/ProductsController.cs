ad75939538b3db1ea24423b92e1f9b9d23582610
garizola <99741841+garizola@users.noreply.github.com>
2024-01-26 19:23:02 -0500

    first commit

---
+++ ContosoCrafts.WebSite/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    [Route("[controller]")]
+    public class ProductsController : Controller
+    {
+       public ProductsController(JsonFileProductService productService)
+       {
+            this.ProductService = productService;
+       }
+
+        public JsonFileProductService ProductService { get;  }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+    }
+}
+
---
+++ ContosoCrafts.WebSite/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddTransient<JsonFileProductService>();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+
+//app.MapGet("/products", (context) =>
+//{
+//    var products = app.ApplicationServices.GetService<JsonFileProductService>().GetProducts();
+//    var json = JsonSerializer.Serialize<IEnumerable<Product>>(products);
+//});
+
+app.Run();
+
